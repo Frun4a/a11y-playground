88f125f95a20870ac7509b3e004e71540b48b423
Nikolai <rivennike@gmail.com>
2021-03-10 08:52:02 -0800

    Added ids to the button, also added display:none to the collapsed accordion sections

--- components/AccordionWithHooks/AccordionH.js
+++ components/AccordionWithHooks/AccordionH.js
@@ -1,4 +1,5 @@
 import React, { useState, useRef } from 'react'
+import nextId from'react-id-generator'
 import Chevron from './Chevron'
 import styles from './AccordionH.module.css'
 
@@ -7,31 +8,42 @@ function AccordionH(props) {
   const [setActive, setActiveState] = useState("")
   const [setHeight, setHeightState] = useState("0px")
   const [setRotate, setRotateState] = useState("")
+  const [setDisplayCss, setDisplayCssState] = useState(styles.display_none)
+  const [setAriaExpanded, setAriaExpandedState] = useState("false")
 
   const content = useRef(null)
+  const buttonId = nextId();
 
   function toggleAccordion() {
     setActiveState(setActive === "" ? styles.active : "")
-    //console.log(content.current.scrollHeight)
 
-    setHeightState(
-      setActive === styles.active ? '0px' : `${content.current.scrollHeight}px`
-    )
+    // setHeightState(
+    //   setActive === styles.active ? '0px' : `${content.current.scrollHeight}px`
+    // )
 
     setRotateState(setActive === styles.active ? "" : styles.rotate)
+
+    setDisplayCssState(setActive === styles.active ? styles.display_none : "")
+
+    setAriaExpandedState(setActive === styles.active ? "false" : "true")
   }
 
   return (
     <div className={styles.accordion_section}>
-      <button className={`${styles.accordion} ${setActive}`} onClick={toggleAccordion}>
+      <button 
+        className={`${styles.accordion} ${setActive}`}
+        onClick={toggleAccordion}
+        aria-expanded={setAriaExpanded}
+        id={buttonId}
+      >
         <p className={styles.accordion_title}>{props.title}</p>
         <Chevron className={`${styles.accordion_icon} ${setRotate}`} width={10} fill="#777" />
       </button>
 
       <div 
         ref={content}
-        className={styles.accordion_content}
-        style={{ maxHeight: setHeight }}
+        className={`${styles.accordion_content} ${setDisplayCss}`}
+        //style={{ maxHeight: setHeight }}
       >
 
         <div
