729134ffe49a9d6a7afcc8374483808bcf8eb2bd
Nikolai <rivennike@gmail.com>
2021-04-07 21:04:50 -0700

    Update the index page.

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,27 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import styles from './index.module.css'
 
 export default function Home() {
   return (
     <Layout>
-
-      <h1 style={{ textAlign: "center" }}>Index page (Work in progress)</h1>
-
+      <div className={styles.top_container}>
+        <div className={styles.inner_container}>
+          <div className={styles.logoimage_container}>
+            <img 
+              src="/in_progress.jpg"
+              alt="The image of a tower crane" />
+          </div>
+          <div className={styles.indexTextContainer}>
+            <div className={styles.indexText}>
+              The website is WIP.
+              <br />
+              This is the place where I am learning React, Next.js and a11y.
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
