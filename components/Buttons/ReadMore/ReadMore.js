c3e1d115e11977a4d8d1336beeeec2a8dffd34a3
Nikolai Kolesnikov (MacBook Pro) <rivennike@gmail.com>
2021-08-30 21:14:07 -0700

    Add the ReadMore component. Add 2 cards to the Buttons & links page.

---
+++ components/Buttons/ReadMore/ReadMore.js
@@ -0,0 +1,99 @@
+import { React, useState, useRef, useEffect } from "react";
+import styles from "./ReadMore.module.css";
+
+function truncateCopy(copy, type, length) {
+  if (copy && ["words", "characters"].includes(type)) {
+    if (type === "characters") {
+       if (copy.length > length) {
+        return copy.substring(0, length) + "...";
+       } else {
+         return copy + "...";
+       }
+    } else {
+      const arrayOfWords = copy.split(' ');
+      if (arrayOfWords.length > length) {
+        arrayOfWords.length = length;
+        const truncatedCopy =  arrayOfWords.join(' ');
+        if ([',', '!', '.', '?', ':', ';'].includes(truncatedCopy.slice(-1))) {
+          return truncatedCopy.slice(0, -1) + '...';
+        } else {
+          return truncatedCopy + '...';
+        }
+      }
+    }
+  }
+}
+
+// ReadMore component accepts:
+// copy: string
+// length: integer
+// type: enum - 'words' or 'characters'
+
+const ReadMore = (props) => {
+  const [expanded, setExpanded] = useState(props.expanded);
+
+  const [copy, setCopy] = useState({
+    fullCopy: props.copy,
+    truncationType: props.type,
+    truncatedCopy: truncateCopy(props.copy, props.type, props.length),
+  });
+
+  const [collapsedHeight, setCollapsedHeight] = useState(null);
+
+  //Set pointer for the div
+  const divRef = useRef(null);
+
+  let buttonCaption = !expanded ? "Read more" : "Read less";
+
+  const handleToggleButtonClick = () => {
+    // Collapsed -> Expanded transition
+    if (!expanded) {
+      // Getting the initial div height
+      const initialDivHeight = divRef.current.clientHeight;
+
+      //Save the initial div height to the state
+      setCollapsedHeight(initialDivHeight);
+
+      // Change div's max-height from auto to its actual height
+      divRef.current.style.maxHeight = initialDivHeight + "px";
+
+      // Expand the section
+      setExpanded(!expanded);
+    } else {
+      //Expanded -> Collapsed transition
+
+      // Collapse the section, but not instantly so the copy doesn't dissappear before the animation starts
+      setTimeout(() => setExpanded(!expanded), 750);
+
+      // Change div's max-height to the previously saved collapsed div height
+      divRef.current.style.maxHeight = collapsedHeight + "px";
+    }
+  };
+
+  useEffect(() => {
+    if (expanded) {
+      // Get the expanded div height using scrollHeight property
+      const endDivHeight = divRef.current.scrollHeight;
+
+      // Change div's height from collapsed div value to its expanded height value
+      divRef.current.style.maxHeight = endDivHeight + "px";
+    }
+  }, [expanded]);
+
+  return (
+    <>
+      <div className={styles.read_more} ref={divRef}>
+        {expanded ? copy.fullCopy : copy.truncatedCopy}
+      </div>
+      <button
+        type="button"
+        onClick={handleToggleButtonClick}
+        className={styles.read_more_less_button}
+      >
+        {buttonCaption}
+      </button>
+    </>
+  );
+};
+
+export { ReadMore };
--- pages/buttons.js
+++ pages/buttons.js
@@ -1,32 +1,67 @@
 import Layout from "../components/Layout";
+import { useState, useEffect } from "react";
 import { CardsContainer, Card } from "../components/CardsGrid/CardsContainer";
 import Head from "next/head";
 
 import ToggleButton from "../components/Buttons/Toggle Button/ToggleButton";
+import { ReadMore } from "../components/Buttons/ReadMore/ReadMore.js";
 import { MenuButton } from "../components/Buttons/Menu Button/MenuButton";
 
 export default function Buttons() {
+  const [charactersCount, setCharactersCount] = useState(200);
+  const [wordsCount, setWordsCount] = useState(42);
+
   return (
     <Layout>
       <Head>
         <title>Buttons and links | A11y playground</title>
       </Head>
       <CardsContainer>
+        <Card title="Read More & Less (characters)">
+          <div style={{ marginBottom: '0.5rem' }}>
+            <label htmlFor="characters" style={{ marginRight: "10px" }}>
+              Number of characters:
+            </label>
+            <input
+              type="number"
+              id="characters"
+              name="characters"
+              min="1"
+              max="500"
+              step="1"
+              value={charactersCount}
+              onChange={(e) => {
+                setCharactersCount(e.target.value);
+              }}
+            />
+          </div>
+          <ReadMore
+            key={charactersCount}
+            copy="But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"
+            length={charactersCount}
+            type="characters"
+          />
+        </Card>
+
+        <Card title="Read More & Less (words)">
+          <ReadMore
+            copy="But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"
+            length={wordsCount}
+            type="words"
+          />
+        </Card>
+
         <Card
           title='Toggle button (uses the "aria-pressed" attribute)'
           dVoiceover='Reads "Toggle button" for the unselected button and "selected, toggle button" for the selected one.
                       When the unselected button is activated, VO reads "selected".
                       When the selected button is activated, VO does not read "unselected" (remains silent).'
         >
-
           <ToggleButton caption="Mute #1" pressed={false} icon={true} />
           <ToggleButton caption="Filters" pressed={true} icon={false} />
         </Card>
 
-        <Card
-          title='Menu button'
-          dVoiceover=''
-        >
+        <Card title="Menu button" dVoiceover="">
           <MenuButton />
         </Card>
 
